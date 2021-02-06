a8ad158db9a2faa4296fe95cfd0be17757606f86
Leo Rondeau <christopherleorondeau@gmail.com>
2021-02-06 12:16:43 -0600

    Added tasklist function to iterate through tasks

---
+++ src/components/task/TaskList.js
@@ -0,0 +1,27 @@
+import React, {useEffect, useContext} from 'react'
+import { TaskContext } from './TaskProvider'
+import { Task }  from './Task'
+
+
+
+export const TaskList = ({props}) => {
+    const {tasks, getTasks} = useContext(TaskContext)
+
+    useEffect(() =>{
+        getTasks()
+    }, [])
+
+    return (
+        <>
+        <section>
+            <div>
+                {
+                    tasks.map(t => <Task key={t.id} task={t} /> )
+                }
+            </div>
+        </section>
+        </>
+
+    )
+
+}
