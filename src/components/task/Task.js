008d35a885f7c99f60fb30e6376992abb0e7f95a
Leo Rondeau <christopherleorondeau@gmail.com>
2021-02-06 16:50:14 -0600

    added a window confirmation for delete

--- src/components/task/Task.js
+++ src/components/task/Task.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect }from 'react'
+import React, { useContext, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { TaskContext } from './TaskProvider'
 
@@ -6,23 +6,30 @@ import { TaskContext } from './TaskProvider'
 
 export const Task = ({ task }) => {
     const { deleteTask, getTasks } = useContext(TaskContext)
-    console.log("task" , task)
     
+
     useEffect(() => {
         getTasks()
-    },[])
+    }, [])
 
+    const deleteConfirm = () => {
+        if (window.confirm("Are you sure you to delete?")) {
+            deleteTask(task.id)
+        } else {
+            
+        }
+    }
 
     return (
         <>
             <section className="task">
                 <div className="task__name">
-                    <Link to={`/${task.id}`}>
+                    <Link to={`/tasks/${task.id}`}>
                         {task.name}
                     </Link>
                 </div>
-                <button onClick={()=> deleteTask(task.id)}>
-                    Delete Task
+                <button onClick={() => deleteConfirm()}>
+                    Delete task
                 </button>
             </section>
         </>
