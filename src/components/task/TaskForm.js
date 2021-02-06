3c3d667f8c6b9cb81a586d729e6455ab8a7b427c
Leo Rondeau <christopherleorondeau@gmail.com>
2021-02-06 15:05:14 -0600

    Added edit functionality

--- src/components/task/TaskForm.js
+++ src/components/task/TaskForm.js
@@ -1,25 +1,26 @@
-import React, {useContext, useEffect, useState} from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { TaskContext } from './TaskProvider'
 
 
 export const TaskForm = (props) => {
     const { tasks, getTasks, addTask, updateTask } = useContext(TaskContext)
     const [task, setTask] = useState({})
-    const editMode = props.match.params.hadOwnProperty("taskId")
+    const editMode = props.match.params.hasOwnProperty("taskId")
 
-    const handleControlledInputStorage = (t) => {
+    const handleControlledInputStorage = (event) => {
 
         const newTask = Object.assign({}, task)
-        newTask[t.target.name] = t.target.value 
+        newTask[event.target.name] = event.target.value
         setTask(newTask)
+
 
     }
 
     const getTaskInEditMode = () => {
-        
+
         if (editMode) {
             const taskId = parseInt(props.match.params.taskId)
-            const selectedTask = tasks.find(t => t.id === taskId) 
+            const selectedTask = tasks.find(t => t.id === taskId)
             setTask(selectedTask)
         }
     }
@@ -28,27 +29,40 @@ export const TaskForm = (props) => {
         getTasks()
     }, [])
 
-    const constructNewTask = () => {
-        const name = task.name
-        
-        addTask ({
-            name
-        })
-    }
+    useEffect(() => {
+        getTaskInEditMode()
+    }, [tasks])
 
+    const constructNewTask = () => {
+
+        if (editMode) {
+            updateTask({
+                id: task.id,
+                name: task.name
+            })
+            .then(() => props.history.push("/"))
+        } else {
+
+            addTask({
+                name: task.name
+            })
+                .then(() => props.history.push("/"))
+
+        }
+    }
     return (
         <>
-        <form className="taskForm">
-            <label className="taskForm__title"></label>
-            <input className="taskForm__title-name" name="title" proptype="varchar" 
-            placeholder="Task To Do" value={task.name} onChange={handleControlledInputStorage}/>
-        </form>
-        <button className="taskForm___button" onClick={tsk => {
-            tsk.preventDefault()
-            constructNewTask()
-        }}>
-            Save
-        </button>
+            <form className="taskForm">
+                <label className="taskForm__title"></label>
+                <input className="taskForm__title-name" name="name" type="text" proptype="varchar"
+                    placeholder="Task To Do" value={task.name} onChange={handleControlledInputStorage} />
+            </form>
+            <button className="taskForm___button" onClick={tsk => {
+                tsk.preventDefault()
+                constructNewTask()
+            }}>
+                {editMode ? "Save Update" : "Save Task"}
+            </button>
         </>
     )
 }
